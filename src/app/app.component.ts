4335f5ec31e31f7e6745042b4bb8d28ce6edb5fb
ryan10328 <ryan10328@gmail.com>
2017-06-10 00:14:51 +0800

    任務 14：注入 Http 服務元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { FooterComponent } from './footer/footer.component';
 import { Component, ViewChild } from '@angular/core';
+import { Http } from "@angular/http";
+
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,13 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http: Http) {
+
+  }
+
   // @ViewChild('myFooter') footer: FooterComponent;
+  isToggleAll: boolean = false;
   selectType: string = 'all';
   inputHint: string = 'What needs to be done??';
   colspan: number = 2;
@@ -15,10 +23,13 @@ export class AppComponent {
   todo: string;
   todos: any[] = [];
 
-  addTodo(evt: KeyboardEvent) {
-    let input = evt.target as HTMLInputElement;
-    this.todos = [...this.todos, { todo: this.todo, done: false }];
-    input.value = '';
+  addTodo() {
+    if (this.todo) {
+      // let input = evt.target as HTMLInputElement;
+      this.todos = [...this.todos, { todo: this.todo, done: false }];
+      // input.value = '';
+      this.todo = '';
+    }
   }
 
   clearCompleted(evt) {
@@ -28,5 +39,17 @@ export class AppComponent {
 
   onSelectType(evt) {
     this.selectType = evt;
+  }
+
+  toggleAll() {
+    this.todos = this.todos.map(item => {
+      return item.done === this.isToggleAll ?
+        { todo: item.todo, done: item.done } :
+        { todo: item.todo, done: !item.done };
+    });
+  }
+
+  deleteTodo(item) {
+    this.todos = this.todos.filter(x => x != item);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
@@ -18,7 +20,8 @@ import { FilterPipe } from './filter.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
