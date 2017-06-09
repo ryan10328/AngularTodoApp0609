b39f4ddfd5b49ed618d47cea43ddf43be83f2ef9
ryan10328 <ryan10328@gmail.com>
2017-06-09 23:21:54 +0800

    任務 10：套用 @Input() 裝飾器 (Decorator)
    實作 OnChanges 介面，並實作 ngOnChanges 方法

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnChanges {
+  moreThanFive: boolean;
 
   @Input('todos') myTodos: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.moreThanFive = this.myTodos.length >= 5;
   }
 
 }
