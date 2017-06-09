3d89313935024792bc09858d136e786cb0093f40
ryan10328 <ryan10328@gmail.com>
2017-06-10 00:33:26 +0800

    任務 15：使用 Http 服務元件呼叫 RESTful APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { FooterComponent } from './footer/footer.component';
 import { Component, ViewChild } from '@angular/core';
 import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/rx";
 
 
 @Component({
@@ -11,7 +13,8 @@ import { Http } from "@angular/http";
 export class AppComponent {
 
   constructor(private http: Http) {
-
+    this.getTodos()
+      .subscribe(data => this.todos = data);
   }
 
   // @ViewChild('myFooter') footer: FooterComponent;
@@ -23,17 +26,40 @@ export class AppComponent {
   todo: string;
   todos: any[] = [];
 
+
+  getTodos(): Observable<any[]> {
+    return this.http.get('http://localhost:3000/todos/')
+      .map(x => x.json());
+  }
+
   addTodo() {
     if (this.todo) {
+
+      this.http.post('http://localhost:3000/todos', { todo: this.todo, done: false })
+        .concatMap(x => this.getTodos())
+        .subscribe(data => this.todos = data);
       // let input = evt.target as HTMLInputElement;
-      this.todos = [...this.todos, { todo: this.todo, done: false }];
+      // this.todos = [...this.todos, { todo: this.todo, done: false }];
       // input.value = '';
       this.todo = '';
     }
   }
 
   clearCompleted(evt) {
-    this.todos = this.todos.filter(x => !x.done);
+    this.todos = this.todos.filter(x => x.done);
+
+    let requestLists: any[] = [];
+
+    this.todos.forEach(item => {
+
+      let obs = this.http.delete(`http://localhost:3000/todos/${item.id}`);
+      requestLists.push(obs);
+
+    });
+
+    Observable.forkJoin(requestLists)
+      .concatMap(data => this.getTodos())
+      .subscribe(data => this.todos = data);
     // this.footer.hello();
   }
 
@@ -44,12 +70,32 @@ export class AppComponent {
   toggleAll() {
     this.todos = this.todos.map(item => {
       return item.done === this.isToggleAll ?
-        { todo: item.todo, done: item.done } :
-        { todo: item.todo, done: !item.done };
+        { todo: item.todo, done: item.done, id: item.id } :
+        { todo: item.todo, done: !item.done, id: item.id };
     });
+    let requestLists: any[] = [];
+
+    this.todos.forEach(item => {
+      let obs = this.http.put(`http://localhost:3000/todos/${item.id}`, { todo: item.todo, done: item.done });
+      requestLists.push(obs);
+    });
+
+    Observable.forkJoin(requestLists)
+      .concatMap(data => this.getTodos())
+      .subscribe(data => this.todos = data);
+
   }
 
   deleteTodo(item) {
-    this.todos = this.todos.filter(x => x != item);
+    // this.todos = this.todos.filter(x => x != item);
+    this.http.delete(`http://localhost:3000/todos/${item.id}`)
+      .concatMap(data => this.getTodos())
+      .subscribe(data => this.todos = data);
+  }
+
+  toggleTodo(item) {
+    this.http.put(`http://localhost:3000/todos/${item.id}`, { todo: item.todo, done: item.done })
+      .concatMap(data => this.getTodos())
+      .subscribe(data => this.todos = data);
   }
 }
